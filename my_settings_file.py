fbc0ee8147fee5c53c0a204ebc02969b68acd087
Olivier HUBERT <olituks@gmail.com>
2023-05-19 08:35:46 +0200

    Delete my_settings_file.py
    repolaced by my_settings.py

--- my_settings_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os.path
-
-#variables definition
-DEFAULT_DOWNLOAD_PAGE = "/add/your/path//" #you should finifh the path with //
-LOGIN = "your MMF login name"
-PWD = "your MMF password"
-SQLITE_DB_NAME = os.path.expanduser("~") + "/target/directory/OPR.db"
