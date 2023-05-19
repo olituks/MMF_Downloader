94825c109e3ed1a906d20f88a4288d9fa442d786
Olivier HUBERT <olituks@gmail.com>
2023-05-19 08:35:26 +0200

    Create my_settings.py
    the settings file

---
+++ my_settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path
+
+#variables definition
+DEFAULT_DOWNLOAD_PAGE = "/add/your/path//" #you should finifh the path with //
+LOGIN = "your MMF login name"
+PWD = "your MMF password"
+SQLITE_DB_NAME = os.path.expanduser("~") + "/target/directory/OPR.db"
